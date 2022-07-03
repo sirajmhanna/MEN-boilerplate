bcec8e7fe7ebe46227bb3a6da7529df288135280
Siraj Mhanna <59828433+sirajmhanna@users.noreply.github.com>
2022-07-03 13:59:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require("http");
 const app = require("./app");
 const port = process.env.PORT | 5000;
+const { Logger } = require("./helpers/logger");
 
 const server = http.createServer(app);
 server.listen(port, () => {
-  console.log("Server is alive", port);
+  const logger = new Logger(new Date().getTime(), "server.js", "listen");
+  logger.info("Server is alive", { port });
 });
