da8cc8d09f38c438fd18d74113af4eb3ba1598aa
Siraj Mhanna <59828433+sirajmhanna@users.noreply.github.com>
2022-07-03 13:59:21 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { Logger } = require("./helpers/logger");
 
 // mongoose configuration
 require("./config/mongoose");
@@ -32,7 +33,19 @@ app.use(express.urlencoded({ extended: false }));
 // parse application/json
 app.use(express.json());
 
-// Routes
+// routes
 app.use("/api/v1/", require("./routes"));
+
+// error handler
+app.use((error, req, res, next) => {
+  const logger = new Logger(new Date().getTime(), "app.js", "error-handler");
+  logger.info("server error", { error: error.toString() });
+
+  return res.status(500).json({
+    status: "fail",
+    code: 500,
+    message: "Server error",
+  });
+});
 
 module.exports = app;
