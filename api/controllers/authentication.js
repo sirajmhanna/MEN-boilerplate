2d41e497858cf5d43478ce9fdacdd09d567031dc
Siraj Mhanna <59828433+sirajmhanna@users.noreply.github.com>
2022-07-09 03:06:11 +0300

    Update authentication.js

--- api/controllers/authentication.js
+++ api/controllers/authentication.js
@@ -6,10 +6,11 @@ exports.register = async (req, res) => {
   const logger = new Logger(req.requestID, "authentication", "register");
   let session;
   try {
-    // check if email exists
+    logger.info("Fetching user by email", { email: req.body.email });
     const isEmailExists = await User.getUserByEmail(req.body.email);
 
     if (isEmailExists) {
+      logger.warn("Email address already exists", { email: req.body.email });
       return res.status(409).json({
         ...CommonResponses.emailExists,
         data: {
@@ -18,11 +19,11 @@ exports.register = async (req, res) => {
       });
     }
 
-    // start transaction
+    logger.info("Starting session", {});
     session = await User.startSession();
     await session.startTransaction();
 
-    // add user
+    logger.info("Adding user", {});
     const user = await User.createUser(
       req.body.firstName,
       req.body.lastName,
@@ -33,7 +34,9 @@ exports.register = async (req, res) => {
       session
     );
 
-    // commit transaction
+    logger.info("Committing transaction :: Returning success response", {
+      _id: user._id,
+    });
     await session.commitTransaction();
     await session.endSession();
 
@@ -41,14 +44,31 @@ exports.register = async (req, res) => {
       ...CommonResponses.successUserRegister,
       data: {
         user: {
+          _id: user._id,
           firstName: user.firstName,
           lastName: user.lastName,
           email: user.email,
           phone: user.phone,
           userType: user.userType,
+          createdAt: user.createdAt,
         },
       },
     });
+  } catch (error) {
+    logger.error("server error", { error: error.toString() });
+    if (session) {
+      await session.abortTransaction();
+      await session.endSession();
+    }
+
+    return res.status(500).json(CommonResponses.serverError);
+  }
+};
+
+exports.login = async (req, res) => {
+  const logger = new Logger(req.requestID, "authentication", "login");
+  let session;
+  try {
   } catch (error) {
     logger.error("server error", { error: error.toString() });
     if (session) {
